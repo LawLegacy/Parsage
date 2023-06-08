e7050798100fb9c17d50982417e84550347a3ec1
Pat Law <pat@lawlegacy.com>
2023-06-08 16:28:54 -0500

    CRUD operations

--- Test Task/DataAccess/BikeFacade.cs
+++ Test Task/DataAccess/BikeFacade.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DataAccess
 {
@@ -14,6 +15,57 @@ namespace DataAccess
             using (TesttaskContext context = new TesttaskContext())
             {
                 result = context.Bikes.ToList();
+            }
+
+            return result;
+        }
+
+        public bool SaveBike(Bike bike)
+        {
+            bool result = true;
+
+            try
+            {
+                using (TesttaskContext context = new TesttaskContext())
+                {
+                    if (bike.Id == 0)
+                    {
+                        context.Bikes.Add(bike);
+                    }
+                    else
+                    {
+                        context.Entry(bike).State = EntityState.Modified;
+                    }
+
+                    context.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool DeleteBike(int id)
+        {
+            bool result = true;
+
+            try
+            {
+                using (TesttaskContext context = new TesttaskContext())
+                {
+                    Bike bike = context.Bikes.FirstOrDefault(b => b.Id == id);
+
+                    context.Entry(bike).State &= ~EntityState.Deleted;
+
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
             }
 
             return result;
